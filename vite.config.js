d5bfda700bfd3590c117f67dac71922559249471
Ciarán McGann <ciaranjmcgann@gmail.com>
2026-03-26 21:38:55 +0000

    vite.config added line hopefully will work now

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  base: '/busproject2026/'
 });
