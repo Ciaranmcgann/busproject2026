16edbd329d4cdea9d0b3c507d7fab39ba4926c00
Ciarán McGann <ciaranjmcgann@gmail.com>
2026-03-26 21:53:40 +0000

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [svelte()],
-  base: '/busproject2026/'
+  base: '/busproject2026/',
+  plugins: [svelte()]
 });
