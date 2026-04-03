f448cb9d3bcef60ecc5f1d8ce03d7c2eeb53fb46
Ciarán McGann <ciaranjmcgann@gmail.com>
2026-04-03 19:45:55 +0100

    fixed map

--- src/Map.svelte
+++ src/Map.svelte
@@ -703,7 +703,7 @@
     });
 
     L.tileLayer(
-      "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+      "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
       {
         maxZoom: 19,
         attribution: "&copy; OpenStreetMap contributors",
